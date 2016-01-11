5f890d7014a4407afcb8616051bfcbed118f572b
Dan Lawrence <dan.lawrence3@gmail.com>
2016-01-11 13:25:21 -0500

    updated functions playToFive and getWinner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -50,7 +50,51 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove === "rock"){
+		switch (computerMove){
+			case "rock":
+				winner = "tie";
+				break;
+			case "paper":
+				winner = "computer";
+				break;
+			case "scissors":
+				winner = "player";
+				break;
+			default:
+				break;
+		}
+	}else if (playerMove === "paper"){
+		switch (computerMove){
+			case "rock":
+				winner = "player";
+				break;
+			case "paper":
+				winner = "tie";
+				break;
+			case "scissors":
+				winner = "computer";
+				break;
+			default:
+				break;
+		}
+	}else if (playerMove === "scissors"){
+		switch (computerMove){
+			case "rock":
+				winner = "computer";
+				break;
+			case "paper":
+				winner = "player";
+				break;
+			case "scissors":
+				winner = "tie";
+				break;
+			default:
+				break;
+		}
+	}else {
+	}
+	
     return winner;
 }
 
@@ -58,8 +102,19 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+	var thisGame = "";
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+	while (playerWins < 5 && computerWins < 5){
+		thisGame = getWinner(getPlayerMove, getComputerMove);
+		if (thisGame === "player"){
+			playerWins += 1;
+		}else if (thisGame === "computer"){
+			computerWins += 1;
+		}else {
+			
+		}
+	}
     return [playerWins, computerWins];
 }
 
